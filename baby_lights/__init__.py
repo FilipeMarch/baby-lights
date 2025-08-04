77b6002da2f80c6648b35fe14f51e7c96db66dee
Filipe Marchesini <filipe.marchesini@gmail.com>
2025-08-04 02:43:47 -0300

    ✨ feat: Initialize Baby Lights App with versioning and logging

---
+++ baby_lights/__init__.py
@@ -0,0 +1,7 @@
+from baby_lights.app import BabyLightsApp
+from baby_lights.logger import logger
+
+__all__ = ['BabyLightsApp']
+__version__ = '0.1.0'
+
+logger.info(f'Baby Lights App version {__version__} initialized.')
